0ef62abf9fb262c7f973c19f3301b649643e028e
Sebastian Zapata Mardini <sezama03@hotmail.com>
2014-10-22 18:16:56 -0500

    Customize with Bootstrap

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,12 @@
+class WelcomeController < ApplicationController
+  before_action :authenticated, only: [:home]
+  def home
+  end
+
+  private
+    def authenticated
+      if user_signed_in?
+      	redirect_to friends_index_path
+      end
+    end
+end
