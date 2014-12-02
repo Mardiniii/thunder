9477ab242fc9f6f022064979eeecb8276d554060
Sebastian Zapata Mardini <sezama03@hotmail.com>
2014-12-02 10:42:06 -0500

    Add welcome_mail method for mailer, welcome_mail HTML template and devise controller to customize them

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,3 +1,9 @@
 class UserMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "masomenost@gmail.com"
+
+  def welcome_email(user)
+  	@user = user
+  	@url = 'https://twitter.com/Mardiniii'
+    mail(to: @user.email, subject: 'Welcome to My Awesome Thunder Site')
+  end
 end
